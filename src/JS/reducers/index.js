b02ff8639a0b278bad8866cd26423a6fbdf23cde
IMAD <shevchenko_imad@live.fr>
2025-05-24 23:07:45 +0100

    Fix userReducer and update store

--- src/JS/reducers/index.js
+++ src/JS/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import prodReducer from "./prodReducer";
 import bookReducer from "./bookReducer";
+import userReducer from "./userReducer"; 
 
 const rootReducer = combineReducers({
   authReducer,
   prodReducer,
   bookReducer,
+  userReducer, 
 });
 
 export default rootReducer;
